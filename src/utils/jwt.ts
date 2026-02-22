e961d11c93b342186acece590b7fce65320aa7b6
alsamiul123 <alsamiul123@gmail.com>
2026-02-22 11:02:50 +0600

    fix:jwt token expired time fixed

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,13 +3,13 @@ import { config } from "../config/env";
 
 export const generateAccessToken = (payload: any) => {
   return jwt.sign(payload, config.jwt_secret, {
-    expiresIn: "15m",
+    expiresIn: config.jwt_expires_in,
   });
 };
 
 export const generateRefreshToken = (payload: any) => {
   return jwt.sign(payload, config.jwt_refresh_secret, {
-    expiresIn: "7d",
+    expiresIn: config.jwt_refresh_expires_in,
   });
 };
 
