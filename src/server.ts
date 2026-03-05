ce669d2c22dd489f533baa3d7c086ac33ac2d08d
alsamiul123 <alsamiul123@gmail.com>
2026-03-05 17:49:36 +0600

    final commit

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { initSocket } from "./config/socket";
 import { startNotificationConsumer } from "./modules/notification/notification.consumer";
 import http from "http";
 import { connectRabbitMQ } from "./queue/rabbitmq";
-
+const server = http.createServer(app);
 const startServer = async () => {
   try {
     await prisma.$connect();
@@ -14,12 +14,12 @@ const startServer = async () => {
     // ✅ Connect RabbitMQ
     await connectRabbitMQ();
     console.log("✅ RabbitMQ connected");
-    // Create HTTP server
-    const server = http.createServer(app);
 
-    server.listen(config.port, () => {
-      console.log(`🚀 Server running on port ${config.port}`);
-    });
+    if (process.env.NODE_ENV !== "production") {
+      server.listen(config.port, () => {
+        console.log(`🚀 Server running on port ${config.port}`);
+      });
+    }
 
     // Initialize WebSocket
     initSocket(server);
@@ -32,6 +32,6 @@ const startServer = async () => {
   }
 };
 
-if (process.env.NODE_ENV !== "production") {
-  startServer();
-}
+process.on("SigINT", () => server.close());
+process.on("SIGTERM", () => server.close());
+startServer();
